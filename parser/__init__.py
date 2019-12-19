4c32e123ce02329ba4b3c97aead4bcab7bf568f9
kpolushin <kpolushin@azul.com>
2019-12-19 11:09:14 +0700

    Relative imports for parser module

--- parser/__init__.py
+++ parser/__init__.py
@@ -1,12 +1,12 @@
-from parser.common.end_line_parser import EndLineParser, NotFoundEndLineError
+from .common.end_line_parser import EndLineParser, NotFoundEndLineError
 
-from parser.func.func_parser import FuncParser
-from parser.func.key_argument import KeyArgument
+from .func.func_parser import FuncParser
+from .func.key_argument import KeyArgument
 
-from parser.logic.and_parser import AndParser, AndParserError
-from parser.logic.char_parser import CharParser, CharParserInitError
-from parser.logic.empty_parser import EmptyParser
-from parser.logic.or_parser import OrParser, OrParserError
-from parser.logic.repeat_parser import RepeatParser, RepeatParserError, RepeatParserValuesError
+from .logic.and_parser import AndParser, AndParserError
+from .logic.char_parser import CharParser, CharParserInitError
+from .logic.empty_parser import EmptyParser
+from .logic.or_parser import OrParser, OrParserError
+from .logic.repeat_parser import RepeatParser, RepeatParserError, RepeatParserValuesError
 
-from parser.common.simple import spaces, space_parser
+from .common.simple import spaces, space_parser
