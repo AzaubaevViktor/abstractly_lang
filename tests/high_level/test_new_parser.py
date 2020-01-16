789a7d75c5cf9c0d444f0ffe26e9b0d2a4747018
Korovin Viktor <sarvash.inc@gmail.com>
2020-01-16 14:48:35 +0700

    Написал тестов для переопределения функций

--- tests/high_level/test_new_parser.py
+++ tests/high_level/test_new_parser.py
@@ -1,10 +1,11 @@
 def test_new_parser(a):
     # _  := @space
     # __ := _+
-    a("power_parser := a:@number __ `^_^` __ b:@number => @power(a, b)")
+    # TODO:
+    a("@ |= a:@number & __ & `^_^` & __ & b:@number => @power(a, b)")
     assert a("2 ^_^ 3") == 8
 
 
 def test_new_op(a):
-    a("@ |= a:@ __ '%)' __ b:@ => a * b - a - b")
+    a("@ |= a:@ & __ & '%)' & __ & b:@ => a * b - a - b")
     assert a("10 %) 20") == 170
---
+++ tests/high_level/test_parsers.py
@@ -0,0 +1,54 @@
+import math
+
+from parser import CharParser, KeyArgument, FuncParser
+from std_parsers import number_expressions
+from std_parsers.common import spaces
+
+
+def test_spaces(a):
+    assert a("@spaces") == spaces
+    assert a("__") == spaces
+
+
+def test_char_parser(a):
+    assert a("`x`") == CharParser('x')
+
+
+def test_str_parser(a):
+    assert a("`xyz`") == CharParser.line('xyz')
+
+
+def test_change_str_parser(a):
+    assert a("`\\t`") == CharParser("\t")
+
+
+def test_smile_parser(a):
+    assert a("`^_^`") == CharParser.line('^_^')
+
+
+def test_key_argument_parser(a):
+    assert a("a:@number") == KeyArgument('a', number_expressions)
+
+
+def test_and_parser(a):
+    assert a("`x` & `y`") == CharParser('x') & CharParser('y')
+
+
+def test_ior_parser(a):
+    a("__test_ior = `x` | `y`")
+    assert a("__test_ior") == CharParser('x') | CharParser('y')
+
+    a("__test_ior |= `z`")
+
+    assert a("__test_ior") == CharParser('x') | CharParser('y') | CharParser('z')
+
+
+def test_func_parser(a):
+    a("__test_func = a:@number => @factorial(a)")
+    p = a("__test_func")
+    assert isinstance(p, FuncParser)
+
+    assert p.parser == KeyArgument('a', number_expressions)
+
+    a("@ = __test_func")
+    assert a("10") == math.factorial(10)
