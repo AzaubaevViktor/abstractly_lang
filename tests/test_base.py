089ba8de82d210841d52369fda6c13d0101c4b6a
kpolushin <kpolushin@azul.com>
2020-01-16 21:23:02 +0700

    Create and parser hash test

--- tests/test_base.py
+++ tests/test_base.py
@@ -3,3 +3,14 @@ from parser import CharParser
 
 def test_char_hash():
     assert hash(CharParser("x")) == hash(CharParser("x"))
+
+
+def test_and_hash():
+    x = CharParser("x")
+    y = CharParser("y")
+
+    a1 = x & y
+    a2 = x & y
+
+    assert hash(a1) == hash(a2)
+
