0143be023f27201a124ebcb5e8a7628d1e331dbb
Korovin Viktor <sarvash.inc@gmail.com>
2020-03-03 11:40:46 +0700

    ABS-54 Перенёс тесты в отдельную папку

--- test/__init__.py
+++ test/__init__.py
@@ -1,13 +1,1 @@
 from .test import TestsManager, raises, TestedService, skip, will_fail
-from .hello import Hello
-from .two_services import One, Two
-from .test_exc import TestException
-from .test_process import TestServiceProcess
-from .test_test import TestManager
-from .test_raises import TestRaises
-
-from .test_handler_method import TestHandlerMethods
-from .test_wrong_handler_message import TestWrongHandlerMessage
-
-from .test_serialize import TestSerialize
-from .test_communicator import TestCommunicator
