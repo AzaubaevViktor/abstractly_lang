7a5eb30d95254a000a21bd2bd0d52c3466bc7f8e
Korovin Viktor <sarvash.inc@gmail.com>
2020-03-07 17:24:32 +0700

    ABS-3 Переписал Proxy на новом комуникаторе

--- service/proxy.py
+++ service/proxy.py
@@ -7,6 +7,9 @@ from core import Attribute
 
 from service import Service, Message, handler, EntryPoint, ServiceRunner
 from service.message import Shutdown
+from service.sio_comm.base import BaseCommunicator
+from service.sio_comm.comm import SIOKey, ClientSioComm
+from service.sio_comm.service import CommunicateManager
 
 
 class _ProxyHelloMessage(Message):
@@ -23,26 +26,30 @@ class ProxyForService(Service):
         self.logger.important(ProxyForService.mro())
         class_ = self.__class__.mro()[1]
         self.logger.important("Start class", class_=class_)
-        self.communicator = SocketIOCommunicator()
-        await self.communicator.run()
+
+        key, self.communicator = await CommunicateManager.new_identity()
         self._run_background(
-            ProcessSpawner.run_process(class_=class_, communicator=self.communicator)
+            ProcessSpawner.run_process(class_=class_, key=key)
         )
 
-        msg = await self.communicator.send_msg(_ProxyHelloMessage())
+        await self.communicator.connect()
 
-        self.logger.warning("UNAUTHORIZED CONNECTION WITH", communicator=self.communicator)
+        self._run_background(
+            self._run_reciever()
+        )
 
-        assert await msg.result() is True
+    async def _run_reciever(self):
+        while True:
+            msg = await self.communicator.recv()
+            self.logger.important("Recv message, put into main_queue")
+            await self.main_queue.put(msg)
 
     async def _found_processor(self, msg: Message):
         return self.communicator.send_msg
 
     async def shutdown(self, message: Message):
         if self.communicator:
-            msg = await self.communicator.send_msg(Shutdown(cause="Remote Shutdown"))
-            self.logger.warning(await msg.result())
-            await self.communicator.close()
+            await self.communicator.disconnect()
 
 
 class ProcessSpawner(Service):
@@ -50,14 +57,14 @@ class ProcessSpawner(Service):
         self.pool = concurrent.futures.ProcessPoolExecutor(max_workers=4)
 
     @handler
-    async def run_process(self, class_: Type[Service], communicator: "BaseCommunicator"):
+    async def run_process(self, class_: Type[Service], key: SIOKey):
         loop = asyncio.get_running_loop()
         self.logger.important("Run in executor",
                               class_=class_)
         result = await loop.run_in_executor(
             self.pool, EntryPoint.main, {
                 'ServiceRunner': [("SetMainService", {'service_class_name': class_.__name__})],
-                'ProxyClient': [("SetCommunicator", {'server_info': communicator.server_info})]
+                'ProxyClient': [("SetCommunicator", {'key': key})]
             }, "RunnedProcess")
 
         self.logger.important("Process class finished", result=result, class_=class_)
@@ -69,7 +76,7 @@ class ProcessSpawner(Service):
 
 
 class SetCommunicator(Message):
-    server_info: "BaseServerInfo" = Attribute()
+    key: SIOKey = Attribute()
 
 
 class ProxyClient(Service):
@@ -77,33 +84,28 @@ class ProxyClient(Service):
         self.communicator = None
 
     @handler(SetCommunicator)
-    async def set_communicator(self, server_info: BaseServerInfo):
+    async def set_communicator(self, key: SIOKey):
         assert self.communicator is None
-        self.communicator = SocketIOCommunicator(server_info)
-        self.logger.important("Start connect to", communicator=server_info)
-        await self.communicator.run()
-        msg = await self.communicator.receive_msg()
-        assert isinstance(msg, _ProxyHelloMessage), msg
-        msg.set_result(True)
+        self.communicator = ClientSioComm(key)
+
+        await self.communicator.connect()
+
         self._run_background(self._run_waiter())
 
     async def _run_waiter(self):
         while True:
-            msg = await self.communicator.receive_msg()
-            self.logger.important("Message received", msg=msg)
-            result = await msg.to.get(msg)
-            self.logger.important("Result received")
-            msg.set_result(result)
+            msg = await self.communicator.recv()
+            self.logger.important("Receive message, put into main_queue", msg=msg)
+
+            await self.main_queue.put(msg)
 
     async def process(self, message: Message):
-        proxy_msg = await self.communicator.send_msg(message)
+        proxy_msg = await self.communicator.send(message)
         return await proxy_msg.result()
 
     async def shutdown(self, message: Message):
         await ServiceRunner.send(Shutdown(cause="Remote shutdown"))
         if self.communicator:
-            msg = await self.communicator.send_msg(Shutdown(cause="Remote shutdown"))
-            self.logger.warning(await msg.result())
-            await self.communicator.close()
+            await self.communicator.disconnect()
 
 
